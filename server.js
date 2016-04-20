8532574d752f24e0a94771d00b565118089eadec
Cade Nichols <cade@cadenichols.com>
2016-04-20 16:04:17 -0700

    end of ws

--- server.js
+++ server.js
@@ -4,18 +4,32 @@ const PORT = process.env.PORT || 3000;
 
 let jade = require('jade');
 let http = require('http');
+let qs = require('qs');
 
 let nodeStatic = require('node-static');
 let file = new nodeStatic.Server('./public')
 
 http.createServer((req, res) => {
+  let html;
+  let qsParts = req.url.split('?');
+  let path = qsParts[0];
+  
+  let query = qs.parse(qsParts[1]);
 
-  switch(req.url) {
-    case '/':
-      let html = jade.renderFile('./views/index.jade', {
-        title: 'Jade App'
+  switch(path) {
+    case '/': {
+      html = jade.renderFile('./views/index.jade', {
+        theme: validateTheme(query.theme)
       });
       res.end(html);
+      break;
+    }
+    case '/contact': {
+      html = jade.renderFile('./views/contact.jade', {
+        theme: validateTheme(query.theme)
+      });
+      res.end(html);
+    }
   }
 
   file.serve(req, res);
@@ -25,3 +39,19 @@ http.createServer((req, res) => {
   if(err) return console.log(err);
   console.log(`Node server listening on port ${PORT}`);
 });
+
+function validateTheme(theme) {
+  if(theme) {
+    theme = theme.toLowerCase();
+  }
+  let themes = [ 'cerulean', 'cosmo', 'cyborg', 'flatly', 'darkly', 'journal', 'lumen', 'paper', 'readable', 'sandstone', 'simplex', 'slate', 'spacelab', 'superhero', 'united', 'yeti'];
+
+  if(themes.indexOf(theme) !== -1) {
+    return theme;
+  } else {
+    return null;
+  }
+}
+
+
+
---
+++ test.js
@@ -0,0 +1,6 @@
+'use strict';
+
+
+var [a, b] = [1,2]
+
+console.log(a, b);
